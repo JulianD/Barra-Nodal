5d48ffe380d09eb5597af419bb99dfc8688d987d
Julian Muñoz <juliand@laneros.com>
2014-10-30 10:11:15 -0500

    Cambios en el orden de algunos sitios

--- javascript.js
+++ javascript.js
@@ -22,10 +22,10 @@
 </ul> \
 <ul class="links-nodal"> \
 <li><a href="http://www.laneros.com">LANEROS</a></li> \
-<li><a href="http://www.gadgeteros.co">GADGETEROS</a></li> \
 <li><a href="http://www.lapelotona.com">LAPELOTONA</a></li> \
-<li><a href="http://www.hwcol.com">HWCOL</a></li> \
 <li><a href="http://www.turket.co">TURKET</a></li> \
+<li><a href="http://www.hwcol.com">HWCOL</a></li> \
+<li><a href="http://www.gadgeteros.co">GADGETEROS</a></li> \
 </ul> \
 </div>';
 	etiqueta_body.insertAdjacentHTML('beforeend', barra_html);
